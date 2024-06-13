56571a24346844a3d4a0d568948726135654da15
igorek <igor-titov-2004@mail.ru>
2024-06-13 13:02:16 +0300

    create project

---
+++ src/main/java/com/example/practice_modsen_shop/PracticeModsenShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_modsen_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeModsenShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeModsenShopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_modsen_shop/PracticeModsenShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_modsen_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeModsenShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

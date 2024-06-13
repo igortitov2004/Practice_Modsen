f7bad118527c3ddcc1b76c5b0ec55ff3cca7d4f9
FedorC <fedor.sap@gmail.com>
2024-06-13 23:29:10 +0300

    application domain v1

---
+++ src/main/java/com/example/practice_modsen_shop/entities/User.java
@@ -0,0 +1,61 @@
+package com.example.practice_modsen_shop.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "u_id")
+    private int id;
+
+    @Column(name = "u_firstname")
+    private String firstname;
+
+    @Column(name = "u_lastname")
+    private String lastname;
+
+    @Column(name = "u_gender")
+    private String gender;
+
+    @Column(name = "u_login")
+    private String login;
+
+    @Column(name = "u_password_hash")
+    private String passwordHash;
+
+    @Column(name = "u_phone_number")
+    private String phoneNumber;
+
+    @Column(name = "u_birth_date")
+    private Date birthDate;
+
+    @Column(name = "u_city")
+    private String city;
+
+    @Column(name = "u_street")
+    private String street;
+
+    @Column(name = "u_house_num")
+    private String houseNumber;
+
+    @Column(name = "u_apartment_num")
+    private String apartmentNumber;
+
+    @Column(name = "u_email")
+    private String email;
+
+
+}

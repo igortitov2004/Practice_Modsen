c907dade90b205162f3573b58e4aaaa814f527d9
Aleksej <man747704@gmail.com>
2024-06-14 11:19:06 +0300

    ProductController+CategoryController+DTOs+MockitoTests

---
+++ src/main/java/com/example/practice_modsen_shop/mappers/ProductMapper.java
@@ -0,0 +1,15 @@
+package com.example.practice_modsen_shop.mappers;
+
+import com.example.practice_modsen_shop.dto.ProductRequestTo;
+import com.example.practice_modsen_shop.dto.ProductResponseTo;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel="spring")
+public interface ProductMapper {
+//    Product toProduct(ProductRequestTo productRequestTo);
+//    ProductResponseTo toProductResponse(Product product);
+//    List<Product> toProductList(List<ProductRequestTo> productListRequestTo);
+//    List<ProductResponseTo> toProductResponseList(List<Product> product);
+}

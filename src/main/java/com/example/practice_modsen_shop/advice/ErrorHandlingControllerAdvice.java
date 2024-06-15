19af76c7349aa97811e29c9c60af6998e5ec1bdf
kauchakoff <kauchakovie@gmail.com>
2024-06-15 04:06:39 +0300

    added 2 error handling and made tests

--- src/main/java/com/example/practice_modsen_shop/advice/ErrorHandlingControllerAdvice.java
+++ src/main/java/com/example/practice_modsen_shop/advice/ErrorHandlingControllerAdvice.java
@@ -1,19 +1,34 @@
 package com.example.practice_modsen_shop.advice;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 import java.lang.reflect.UndeclaredThrowableException;
+
+import jakarta.validation.ConstraintViolationException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import java.util.ConcurrentModificationException;
 import java.util.EmptyStackException;
 import java.util.MissingResourceException;
 import java.util.NoSuchElementException;
 
-@ControllerAdvice
-public class DefaultExceptionHandler {
 
+
+
+
+@ControllerAdvice
+public class ErrorHandlingControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<String> handleException(ArithmeticException e){
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -112,6 +127,50 @@ public class DefaultExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<String> handleException(UnsupportedOperationException e){
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onConstraintValidationException(ConstraintViolationException e) {
+        final List<Violation> violations = e.getConstraintViolations().stream()
+                .map(violation -> new Violation(violation.getPropertyPath().toString(), violation.getMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+    @ResponseBody
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ValidationErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        final List<Violation> violations = e.getBindingResult().getFieldErrors().stream()
+                .map(error -> new Violation(error.getField(), error.getDefaultMessage()))
+                .collect(Collectors.toList());
+        return new ValidationErrorResponse(violations);
+    }
+
+
+
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class ValidationErrorResponse {
+        private final List<Violation> violations;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class Violation {
+        private final String fieldName;
+        private final String message;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public static class ErrorResponse {
+        private final int status;
+        private final String message;
+        private final LocalDateTime timestamp;
     }
 
 }

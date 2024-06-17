8e6864d3cd23e204108c04d75d1bed1823b61cc4
Aleksej <man747704@gmail.com>
2024-06-17 11:32:37 +0300

    Tests for JPA repositories(CategoryRepository, ProductRepository).

---
+++ src/test/java/com/example/practice_modsen_shop/CategoryRepositoryTest.java
@@ -0,0 +1,118 @@
+package com.example.practice_modsen_shop;
+
+import com.example.practice_modsen_shop.entities.Category;
+import com.example.practice_modsen_shop.repository.CategoryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void testCreateCategoryAndFindById() {
+        Category toSaveCategory = new Category();
+        toSaveCategory.setName("Saved category");
+
+        categoryRepository.save(toSaveCategory);
+
+        assertTrue(categoryRepository.findById(toSaveCategory.getId()).isPresent());
+        assertTrue(toSaveCategory.getName().contentEquals(categoryRepository.findById(toSaveCategory.getId()).get().getName()));
+    }
+
+    @Test
+    void testUpdateCategory() {
+        Category newCategory = new Category();
+        String newName = "changed";
+
+        newCategory.setName("New category");
+
+        categoryRepository.save(newCategory);
+        long categoryID = newCategory.getId();
+
+        newCategory.setName(newName);
+        categoryRepository.save(newCategory);
+
+        assertTrue(categoryRepository.findById(categoryID).isPresent());
+        assertTrue(categoryRepository.findById(categoryID).get().getName().contentEquals(newCategory.getName()));
+        assertEquals(newCategory.getId(), categoryID);
+    }
+
+    @Test
+    void testDeleteByIdCategory() {
+        Category toDeleteCategory = new Category();
+        toDeleteCategory.setName("to delete");
+
+        categoryRepository.save(toDeleteCategory);
+        categoryRepository.deleteById(toDeleteCategory.getId());
+
+        assertNull(categoryRepository.findById(toDeleteCategory.getId()).orElse(null));
+    }
+
+    @Test
+    void testDeletedCategory() {
+        Category toDeleteCategory = new Category();
+        toDeleteCategory.setName("to delete");
+
+        categoryRepository.save(toDeleteCategory);
+        categoryRepository.delete(toDeleteCategory);
+
+        assertTrue(categoryRepository.findById(toDeleteCategory.getId()).isEmpty());
+    }
+
+    @Test
+    void testPaginationAndSorting() {
+        List<Category> categories;
+
+        categoryRepository.deleteAll();
+
+        Category category = new Category();
+        category.setName("Cat1");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat2");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat3");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat4");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat5");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat6");
+        categoryRepository.save(category);
+
+        category = new Category();
+        category.setName("Cat7");
+        categoryRepository.save(category);
+
+        categories = new ArrayList<>(categoryRepository.findAll());
+
+        List<Category> result = categoryRepository.findAll(PageRequest.of(0, categories.size(), Sort.Direction.ASC, "name")).toList();
+        categories.sort(Comparator.comparing(Category::getName));
+        assertTrue(IntStream.range(0, categories.size())
+                .allMatch(i -> categories.get(i).getId().equals(result.get(i).getId())));
+
+        categoryRepository.deleteAll();
+    }
+}

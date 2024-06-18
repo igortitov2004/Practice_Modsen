2fee84a80196f122394894f83ae7c0eb91279c9c
Simurana <nprimydraya11@gmail.com>
2024-06-18 12:54:36 +0300

    remove tests, correct mistakes

---
+++ src/main/java/com/modsen/practice/exceptionhandler/ValidationErrorResponse.java
@@ -0,0 +1,12 @@
+package com.modsen.practice.exceptionhandler;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class ValidationErrorResponse {
+    private final List<Violation> violations;
+}

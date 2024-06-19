f0a6d750de0ffd00c2537a86dfa4a169b681261f
igorek <igor-titov-2004@mail.ru>
2024-06-19 11:28:06 +0300

    refactor request mapping url

--- src/main/java/com/modsen/practice/auth/AuthenticationController.java
+++ src/main/java/com/modsen/practice/auth/AuthenticationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
 
--- src/main/java/com/modsen/practice/configuration/SecurityConfiguration.java
+++ src/main/java/com/modsen/practice/configuration/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/auth/**")
+                        .requestMatchers("/api/auth/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
